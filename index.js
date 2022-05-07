e9811561cb9a09be306ad0e7a70ecbfbd700443b
Imesha Basnayake <81801951+Imesha599@users.noreply.github.com>
2022-05-08 01:27:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,175 @@
+function allfill() {
+    var err = document.getElementById("error");
+    var fname = document.getElementById("f-name").value;
+    var lname = document.getElementById("l-name").value;
+	var gender = document.getElementById("gender").value;
+    var bday = document.getElementById("bday").value;
+    var age = document.getElementById("age").value;
+    var email = document.getElementById("email").value;
+    var username = document.getElementById("username").value;
+    var pwd = document.getElementById("pwd").value;
+    var confo_pwd = document.getElementById("confo-pwd").value;
+    var mer= document.getElementById("mer").checked;
+
+    if (
+        fname == "" ||
+        lname == "" ||
+		gender== "" ||
+        bday == "" ||
+        age == "" ||
+        email == "" ||
+        username == "" ||
+        pwd == "" ||
+        confo_pwd == "" ||
+        mer == false
+    ) {
+        err.innerHTML = "please fill all fields before submition";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+
+
+function maxChar() {
+    var fname = document.getElementById("f-name");
+    var lname = document.getElementById("l-name");
+    var err = document.getElementById("error");
+    if (fname.value.length > 30 || lname.value.length > 30){
+        err.innerHTML = "please limit your first name and last name to 30";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+function Calage() {
+    var bday = document.getElementById("bday");
+    var age_in = document.getElementById("age");
+
+    var today = new Date();
+    var birthDate = new Date(bday.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    age_in.value = age;
+}
+
+function validateForm() {
+    if (!allfill() ||
+        !maxChar() ||
+        !validateEmail() ||
+        !userName() ||
+        !pwdValidation() ||
+        !pwdMatch() ||
+        !condition()
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateEmail() {
+    var err = document.getElementById("error");
+    var email = document.getElementById("email").value;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var stat = re.test(String(email).toLowerCase());
+    if (!stat) {
+        err.innerHTML = "please Enter Valid Email Address";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+
+
+function userName() {
+    var err = document.getElementById("error");
+    var username = document.getElementById("username");
+    val = username.value;
+    if (!/^[a-z]+$/.test(val) || val.length > 10) {
+        err.innerHTML =
+            "Please use  single case letters only , should contain only 10 characters and don't put spaces";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+
+function pwdValidation() {
+    var err = document.getElementById("error");
+    var pwd = document.getElementById("pwd");
+    var val = pwd.value;
+    if (val.length > 15 || val.length < 6 || !val.match(/[0-9]/g)) {
+        err.innerHTML =
+            "Password should contain letters and at least one number and length should be between 6 to 15";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+
+function pwdMatch() {
+    var err = document.getElementById("error");
+    var pwd = document.getElementById("pwd");
+    var confo_pwd = document.getElementById("confo-pwd");
+    if (pwd.value.localeCompare(confo_pwd.value) != 0) {
+        err.innerHTML = "Password doesn't match";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        return true;
+    }
+}
+
+function condition() {
+    var err = document.getElementById("error");
+    var mer = document.getElementById("mer");
+    var bu = document.getElementById("su-button");
+    console.log(ag.checked);
+    if (!ag.checked) {
+        err.innerHTML = "Please agree into the terms and Conditions";
+        err.classList.remove("hide");
+        err.classList.add("error");
+        bu.disabled = true;
+        return false;
+    } else {
+        err.innerHTML = "";
+        err.classList.add("hide");
+        err.classList.remove("error");
+        bu.disabled = false;
+        return true;
+    }
+}
